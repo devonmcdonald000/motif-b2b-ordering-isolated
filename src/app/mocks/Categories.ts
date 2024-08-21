f9889a947cab7969a70e8fa8f5822a57dcd7e2c8
devonmcdonald000 <devon.mcdonald@aeroflowinc.com>
2024-08-21 09:49:54 -0500

    add mock images

--- src/app/mocks/Categories.ts
+++ src/app/mocks/Categories.ts
@@ -1,22 +1,22 @@
 export const CategoriesMock = [
   {
     name: "Breast Pumps",
-    image: "https://s3-alpha-sig.figma.com/img/38d0/347b/1b74780ab61cbc8411ea6f20c82c7530?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K87YrxhGMgsYFpm1MjFjKOQEZFhetlQVvtbzTRj7BlrX2OlQIpm5fWGJspWKZDDG-dJJKvz-Lik-XNOAFb2LfSyYH7bfbFkR8diV1RE05vfnslFYOW74UHZTq2216iDNWk6cwS5WH-LybWrvvCeWif-5HYD3ItBPMa0QXHk1nHxurZ8Hx6HnOwQD4rDo7~Z4O7rmho~RPTbsRM5h0cWiZ8iYXCtsuOdM~N-6nOsjb8poiT0N5j6Gx1VZgykFDQBAlL-uzgCiZv1~7ZW5j1~R1r0QWMexeR0AyioNMMGMtFnoKSLEdG5rgJz44pa~XOfSNN4Ki6u9B5tx8U~3UFvM9A__"
+    image: "/mock_images/Pumps.png"
   },
   {
     name: "Milk Storage",
-    image: "https://s3-alpha-sig.figma.com/img/58a3/16fc/b0044626c5210bee091a04f9ba798377?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=d~FmqKebhIiKQyJqV99Wc18ThcbrkJH7jNOMUyqq60y8ItRCdD9Yvgq564OZpTxqyhNK7QLqI08auHZhs1ztKefD7wJ0j~pNzfJAOHABQcs4Jgc~0IJvZxkUm20Ma-rW2G1nCU3qzgCMwQ1avNgc1vOrDqeEeM7nTzkUYNk5UxBu0VbhxxXFx6Sv-VovSSnJWrHYN8qnMz8nO~bWhdFm5ccnwp8z9VFRuljBe3wqDTL-m3lFybCXhJ7-IH0sRivrFmmQocngJ2PR1-up6x8Jf2u~2Nq61QDD6DyYCcLfMWYhZOU3RrBu6ohvNUbiIzVCkRrebiMU7vk178wy5YHoBw__"
+    image: "/mock_images/Resupply.png"
   },
   {
     name: "Compression",
-    image: "https://s3-alpha-sig.figma.com/img/7221/d217/131fbd1b30ac3ac65461a35f146f3a37?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MxFckI9-y2pjFeOO3DdxSIjO2rsnuIamg0dySDv4y0OiQ0j7ItkGsHV87ySDSVgXo096F417fl7YQ4BBk14CSx76B3YOxOWD1uOI6~xU12c7swiXQ1X1OpUpMOrjVjJ-mMxw3grhNUTkPOW-V5hi4NP4BsD2Snx13aG9oLkOXEPrRsv85A4tmWU~t-~ymRWgMzcv-ypKxTaHG8qSODk95ipyqTIo~la2USz0MEeTUm3UR8MiiRZsw60vwyGC-Zg1XMYTpYSgM~AQfnMTQEX8Wo75RfHxajIlIBApkrpaA6SzYwocTvYs3tzGpCauGUUuM~Od2X6BtWT3QNXBlomr6w__"
+    image: "/mock_images/Compression.png"
   },
   {
     name: "Maternity Wound Care",
-    image: "https://s3-alpha-sig.figma.com/img/12d4/4c63/e2c8e4d510446e2ad371360e2a31840f?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SyQwEELHtJGGug-ry4w2EeNcCHTJTg7F7a8~FcFNGuDqxxqVuVicCPU7eY-cmK3Cpajjt7W~3eoAbgccfrGWDmuZgdcfRtP7O2voM1AlULZ6bVWHH7wMmJbGe9EJNvMAljtJizbSDZsTV8RN8ckV-Qxv2CEA4jESeF0kwhqJRUdB2ui7JkU7CLMEcDPCR0kJhfc6-DVF~XDu6AHAgT8Mqr44gEOAIN6YZy0NEvZ2p0WQg4hZXP09OBYxV8~AJ4kq1ccgJf6gXaWp~1UnPFqIj5hahZBrhcQUAPr6hwVC021q9KLaXFJJ2gNbxYl5qL8d6NWadpFxGmoJJw-zkk3BJw__"
+    image: "/mock_images/Maternity_Wound.png"
   },
   {
     name: "Phototherapy",
-    image: "https://s3-alpha-sig.figma.com/img/68b9/3a1d/ec14d833e70ceffc66a583d9d6d5075e?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hlDfyDPqbucLm7XYOf-HbW-1s2I0I7wzaaoNvsqu~v0GGDGwHWaTzybMlyKWvmocDHHD1WIS8-zesYtFaus0l3kBtZtnBP2qtL83zG74ttx9DXJHtm88KIhwGfJHWWbdV2xWDUKEiLqrmVBMU5wUIct5cMpOt7hCXB94z2RtuGye~iLfygIb-XJ7QAipKXYkzZKOEZO4XW9NjlOT1KQIMwhFTejfXzo6GwPz4QeAtR1bslCR-e-cYZlc9KZc2kvrcCcQDa4Ti-2VvcOb~oSa9mZwiMtM899U0teO0wHGlNwyhQ0XST2Bmjgia-j8TVXYHkb0Pum2JZESct6g-C1nxw__"
+    image: "/mock_images/Phototherapy.png"
   }
 ]
--- src/app/mocks/Product.ts
+++ src/app/mocks/Product.ts
@@ -6,7 +6,7 @@ export const Products = [
     price_per_unit: 73.99,
     price_per_case: 443.94,
     description: "Sold by the case. Set of 4.",
-    image: "https://s3-alpha-sig.figma.com/img/107d/8cdb/33e807ce9b27d97f5ddebbc9f2113c44?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MqF8TLtbkM0-ZidldUyo7DZaZeH2UheL-2betHAPbPDdDpBAcAfNm6OI-WVJHsFTlmJozYh1pjnYDHkNAlY8UpowJruXGlcjZ4B4rIUU-UIt-zyp-5k~YEEWif1iRsvnuHB~cDkbI0J0NIX5OSi1glZLw3Z8Py1riLvPgCs7Elh9tb-nk5uwOAa6CAZ7R7N~xvKHxu7YeqVUQytAB6qW58pg56~ZUKLAOSE2EEbP3sjuD8l31~XS09z3tGrku45z6rMqsVdciGQtuhuKWE2S4zL5HfkrBWM3kjoVyi2Je1uWlmM2a7sMy-mwHXDatNyidtedeiF8XAbsCLSbBMcbEA__",
+    image: "/mock_images/Duo.png",
     category: "Breast Pumps"
   },
   {
@@ -16,7 +16,7 @@ export const Products = [
     price_per_unit: 73.99,
     price_per_case: 443.94,
     description: "Sold by the case. Set of 4.",
-    image: "https://s3-alpha-sig.figma.com/img/107d/8cdb/33e807ce9b27d97f5ddebbc9f2113c44?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MqF8TLtbkM0-ZidldUyo7DZaZeH2UheL-2betHAPbPDdDpBAcAfNm6OI-WVJHsFTlmJozYh1pjnYDHkNAlY8UpowJruXGlcjZ4B4rIUU-UIt-zyp-5k~YEEWif1iRsvnuHB~cDkbI0J0NIX5OSi1glZLw3Z8Py1riLvPgCs7Elh9tb-nk5uwOAa6CAZ7R7N~xvKHxu7YeqVUQytAB6qW58pg56~ZUKLAOSE2EEbP3sjuD8l31~XS09z3tGrku45z6rMqsVdciGQtuhuKWE2S4zL5HfkrBWM3kjoVyi2Je1uWlmM2a7sMy-mwHXDatNyidtedeiF8XAbsCLSbBMcbEA__",
+    image: "/mock_images/Duo.png",
     category: "Breast Pumps"
   },
   {
@@ -26,7 +26,7 @@ export const Products = [
     price_per_unit: 73.99,
     price_per_case: 443.94,
     description: "Sold by the case. Set of 4.",
-    image: "https://s3-alpha-sig.figma.com/img/107d/8cdb/33e807ce9b27d97f5ddebbc9f2113c44?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MqF8TLtbkM0-ZidldUyo7DZaZeH2UheL-2betHAPbPDdDpBAcAfNm6OI-WVJHsFTlmJozYh1pjnYDHkNAlY8UpowJruXGlcjZ4B4rIUU-UIt-zyp-5k~YEEWif1iRsvnuHB~cDkbI0J0NIX5OSi1glZLw3Z8Py1riLvPgCs7Elh9tb-nk5uwOAa6CAZ7R7N~xvKHxu7YeqVUQytAB6qW58pg56~ZUKLAOSE2EEbP3sjuD8l31~XS09z3tGrku45z6rMqsVdciGQtuhuKWE2S4zL5HfkrBWM3kjoVyi2Je1uWlmM2a7sMy-mwHXDatNyidtedeiF8XAbsCLSbBMcbEA__",
+    image: "/mock_images/Luna.png",
     category: "Milk Storage"
   },
   {
@@ -36,7 +36,7 @@ export const Products = [
     price_per_unit: 73.99,
     price_per_case: 443.94,
     description: "Sold by the case. Set of 4.",
-    image: "https://s3-alpha-sig.figma.com/img/107d/8cdb/33e807ce9b27d97f5ddebbc9f2113c44?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MqF8TLtbkM0-ZidldUyo7DZaZeH2UheL-2betHAPbPDdDpBAcAfNm6OI-WVJHsFTlmJozYh1pjnYDHkNAlY8UpowJruXGlcjZ4B4rIUU-UIt-zyp-5k~YEEWif1iRsvnuHB~cDkbI0J0NIX5OSi1glZLw3Z8Py1riLvPgCs7Elh9tb-nk5uwOAa6CAZ7R7N~xvKHxu7YeqVUQytAB6qW58pg56~ZUKLAOSE2EEbP3sjuD8l31~XS09z3tGrku45z6rMqsVdciGQtuhuKWE2S4zL5HfkrBWM3kjoVyi2Je1uWlmM2a7sMy-mwHXDatNyidtedeiF8XAbsCLSbBMcbEA__",
+    image: "/mock_images/Duo.png",
     category: "Breast Pumps"
   },
 ]
